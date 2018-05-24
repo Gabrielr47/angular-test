9d484e20e7dbaf3ea81cd9e555233a09f215951d
Gabriel Rodrigues <gabrielr47@gmail.com>
2018-05-24 00:28:47 -0300

    05 Setup and Tear Down

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,19 @@
-import { VoteComponent } from './vote.component'; 
+import { VoteComponent } from './vote.component';
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let vote: VoteComponent;
+
+  beforeEach(() => {
+    vote = new VoteComponent();
   });
 
-  it('', () => {
+  it('Should increment totalVotes when upvoted', () => {
+    const result = vote.upVote();
+    expect(vote.totalVotes).toBe(1);
+  });
+
+  it('Should decrement totalVotes when downVoted', () => {
+    const result = vote.downVote();
+    expect(vote.totalVotes).toBe(-1);
   });
 });
